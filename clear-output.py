424df778b6714a6bf4aa3fdece8bcbdc89c458ce
Jianghui Ouyang <49295179+ohyan@users.noreply.github.com>
2022-04-20 20:10:33 +0900

    refactoring

--- clear-output.py
+++ clear-output.py
@@ -1,7 +1,6 @@
 import sys
 import json
 import logging
-import os
 import copy
 from typing import Dict
 import click
@@ -16,21 +15,25 @@ logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.DEBUG)
 def main(jupyter_file, not_clear_signal):  
     # read json
     try:
-        f_open = open(jupyter_file, 'r')
-        jupyter_json = json.load(f_open)
+        with open(jupyter_file, 'r') as f:
+            jupyter_json = json.load(f)
+        f.close()
     except Exception as err:
         logging.error('error opening file, {}'.format(err))
-        return 1
-    f_open.close()
+        exit(1)
 
     # write cleared json
     cleared_jupyter_json = clear_jupyter_output(jupyter_json, not_clear_signal)
     if cleared_jupyter_json is not None:
-        f_write = open(jupyter_file, 'w')
-        json.dump(cleared_jupyter_json, f_write)
+        try:
+            with open(jupyter_file, 'w') as f:
+                json.dump(cleared_jupyter_json, f)
+            f.close()
+        except Exception as err:
+            logging.error('error writing file, {}'.format(err))
     else:
         logging.error('could not process jupyter file')
-        return 1
+        exit(1)
 
     return 0
 
@@ -41,17 +44,17 @@ def clear_jupyter_output(jupyter_json: Dict, not_clear_signal='commit-output') -
     While remain cell outputs with 'cimmit-outpt' in source cell.
     """
     cleared_jupyter_json = copy.deepcopy(jupyter_json)
-    if 'cells' in cleared_jupyter_json:
+    if 'cells' not in cleared_jupyter_json:
+        return None
+    else:
         for cell in cleared_jupyter_json['cells']:
-            if 'outputs' in cell.keys() and 'source' in cell.keys():
+            if 'source' not in cell.keys():
+                return None
+            else:
                 if len(cell['source']) > 1 and not_clear_signal in cell['source'][0]:
                     continue
                 else:
                     cell['outputs'] = []
-            else:
-                return None
-    else:
-        return None
     return cleared_jupyter_json
 
 
