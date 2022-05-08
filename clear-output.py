2005d1642675813a8ce17906fc22f6b6c1ef0791
ohyan <49295179+ohyan@users.noreply.github.com>
2022-05-08 21:54:54 +0900

    refactoring

--- clear-output.py
+++ clear-output.py
@@ -14,24 +14,14 @@ logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.DEBUG)
 @click.option('--jupyter_file', help='jupyter notebook neet to be cleared output')
 @click.option('--not_clear_signal', help='not clear cell signal', default='commit-output')
 def main(jupyter_file, not_clear_signal):  
-    # read json
-    try:
-        f_open = open(jupyter_file, 'r')
-        jupyter_json = json.load(f_open)
-    except Exception as err:
-        logging.error('error opening file, {}'.format(err))
-        return 1
-    f_open.close()
+    with open(jupyter_file, 'r') as f_reader:
+        jupyter_json = json.load(f_reader)
+    f_reader.close()
 
-    # write cleared json
     cleared_jupyter_json = clear_jupyter_output(jupyter_json, not_clear_signal)
-    if cleared_jupyter_json is not None:
-        f_write = open(jupyter_file, 'w')
-        json.dump(cleared_jupyter_json, f_write)
-    else:
-        logging.error('could not process jupyter file')
-        return 1
 
+    with open(jupyter_file, 'w') as f_writer:
+        json.dump(cleared_jupyter_json, f_writer)
     return 0
 
 
@@ -42,7 +32,8 @@ def clear_jupyter_output(jupyter_json: Dict, not_clear_signal='commit-output') -
     """
     cleared_jupyter_json = copy.deepcopy(jupyter_json)
     for cell in cleared_jupyter_json['cells']:
-        if len(cell['source']) > 1 and not_clear_signal in cell['source'][0]:
+        if not_clear_signal in cell['source'][0]:
+            # not clear cell output when not_clear_signal is in the cell
             continue
         else:
             cell['outputs'] = []
